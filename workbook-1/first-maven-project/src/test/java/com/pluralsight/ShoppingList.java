bd1208219679ace558a94e9148ae4db59fa66b5a
Student <enesyilmaz4525@gmail.com>
2024-09-25 13:26:04 -0400

    initial commit

---
+++ workbook-1/first-maven-project/src/test/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Apples");
+
+
+        System.out.println("Salad");
+        System.out.println("tomatoes");
+        System.out.println("bananas");
+        System.out.println("watermelon");
+        System.out.println("mellon");
+        System.out.println("strawberry");
+        System.out.println("blueberry");
+        System.out.println("kiwi");
+        System.out.println("lettuce");
+
+
+
+    }
+}
