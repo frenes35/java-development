268286d049a48d4c2ff377be69a3212996f76758
Student <enesyilmaz4525@gmail.com>
2024-10-01 11:11:53 -0400

    nameexercise

---
+++ workbook-1/SandwichShop/SandwichShop.java
@@ -0,0 +1,2 @@
+public class SandwichShop {
+}
---
+++ workbook-2/FullNameApplication/src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,49 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter your name\nFirst name: ");
+        String firstname = scanner.nextLine().trim();
+
+        System.out.print("Middle name: ");
+        String middleName = scanner.nextLine().trim();
+
+        System.out.print("Last name: ");
+        String lastName = scanner.nextLine().trim();
+
+        System.out.print("Suffix: ");
+        String suffix = scanner.nextLine().trim();
+
+        String fullname = firstname;
+
+
+
+        if (!middleName.isEmpty()) {
+            fullname = fullname + " " + middleName;
+        }
+
+
+        if (!lastName.isEmpty()) {
+            fullname = fullname + " " + lastName;
+
+        }
+        if (!suffix.isEmpty()) {
+            fullname = fullname + " " + suffix;
+
+        }
+
+
+
+        System.out.print("Fullname:" + fullname);
+
+
+
+
+
+
+    }
+}
